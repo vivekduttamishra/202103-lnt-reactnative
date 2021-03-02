ad62ae30fc5411de497b76efe5653d009a0f0c09
Vivek Dutta Mishra <vivek@conceptarchitect.in>
2021-03-02 12:34:59 +0530

    day#2-update#2

--- js-demos/adv-js/books.js
+++ js-demos/adv-js/books.js
@@ -1,6 +1,7 @@
 
-let Book=function(title,author,price,rating,tags){
-
+//ES5 constructor to create book object
+let Book=function(isbn,title,author,price,rating,tags){
+    this.isbn=isbn;
     this.title=title;
     this.author=author;
     this.price=price;
@@ -8,10 +9,11 @@ let Book=function(title,author,price,rating,tags){
     this.tags=tags;
 }
 
+//ES2015 class syntax
 class BookManager{
 
     constructor(){
-        this.books=[];
+        this.books=[]; //empty array of books
     }
 
     addBook(book){
@@ -22,32 +24,62 @@ class BookManager{
         return this.books;
     }
 
+    getBookByIsbn(isbn){
+        for(let book of this.books)
+            if(book.isbn === isbn)
+                return book;
+
+        return null;
+    }
+
+    getBooksByAuthor(author){
+        let result=[];
+        for(let book of this.books){
+            if(book.author===author)
+                result.push(book);
+        }
+
+        return result;
+    }
+
+    getBookInPriceRange(min,max){
+        let result=[];
+        for(let book of this.books){
+            if(book.price>=min && book.price<max)
+                result.push(book);
+        }
+
+        return result;
+    }
     
 }
 
 
 const listBooks=(title='',books)=>{
-    console.log("price\trating\tauthor\title");
-    for(let book of books)
-        console.log(`${book.price}\t${book.rating}\t${book.author}\t${book.title}`);
+    console.log("isbn\tprice\trating\tauthor\title");
+    for(let book of books){
+        let {isbn,title,price,rating,author}=book;
+        console.log(`${isbn}\t${price}\t${rating}\t${author}\t${title}`);
+    }
 }
 
 let manager=new BookManager();
 
-manager.addBook(new Book("The Accursed God","Vivek Dutta Mishra",336,4.6,"india, epic, mahabharata"));
-manager.addBook(new Book("The Sons of Fortune","Jeffrey Archer",400,4.5,"england, politics"));
-manager.addBook(new Book("Runaway Jury","Jeffrey Archer",200,4.7,"fiction, legac"));
-manager.addBook(new Book("Sins of Father","Jeffrey Archer",350,4.6,"chronology, fiction"));
-manager.addBook(new Book("The Firm","John Grisham",150,4.1,"legal fiction"));
-manager.addBook(new Book("Summons","John Grisham",200,3.7,"legal fiction"));
+manager.addBook(new Book("1","The Accursed God","Vivek Dutta Mishra",336,4.6,"india, epic, mahabharata"));
+manager.addBook(new Book("2","The Sons of Fortune","Jeffrey Archer",400,4.5,"england, politics"));
+manager.addBook(new Book("3","Runaway Jury","John Grisham",200,4.7,"fiction, legac"));
+manager.addBook(new Book("4","Sins of Father","Jeffrey Archer",350,4.6,"chronology, fiction"));
+manager.addBook(new Book("5","The Firm","John Grisham",150,4.1,"legal fiction"));
+manager.addBook(new Book("6","Summons","John Grisham",200,3.7,"legal fiction"));
 
 listBooks('All Books', manager.getAll());
 
 
-let totalCost= manager.getAll().reduce( (total,{price})=>{ 
-    
-    return total+price;
-}, 0 );
-console.log('total cost',totalCost);
+let booksByJohnGrisham = manager.getBooksByAuthor("John Grisham");
+
+listBooks('Books by John Grisham', booksByJohnGrisham);
 
 
+let lowCostBook= manager.getBookInPriceRange(0,300);
+
+listBooks('Books under 300', lowCostBook);
