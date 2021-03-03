9b6ca235f2220f88c7f66593e3e4b40f09825a51
Vivek Dutta Mishra <vivek@conceptarchitect.in>
2021-03-03 10:02:45 +0530

    day#3-update#1

--- books/App.js
+++ books/App.js
@@ -10,9 +10,7 @@ let titleStyle={
     fontSize:40
 }
 
-//what UI should my application display
-//whatever default export App.js provides is used as 
-//main application for my React Native
+
 export default function () {
 
     let title='Book App';
@@ -20,10 +18,10 @@ export default function () {
     let subTitle='Book List';
 
     return (
-                <View style={viewStyle} >
-                    <Text style={titleStyle}>{title}</Text>
+            <View style={viewStyle} >
+                <Text style={titleStyle}>{title}</Text>
 
-                    <Text style={{color:'red'}}>{subTitle}</Text>
-                </View>
+                <Text style={{color:'red'}}>{subTitle}</Text>
+            </View>
    );
 }
