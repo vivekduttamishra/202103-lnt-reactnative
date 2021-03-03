4f2c71baaa1dee1d63c8793a809c6ba46561100f
Vivek Dutta Mishra <vivek@conceptarchitect.in>
2021-03-03 13:40:20 +0530

    day#3-update#2

--- books/App.js
+++ books/App.js
@@ -1,27 +1,138 @@
-import React from 'react';
-import { Text, View, SafeAreaView } from 'react-native';
+import React from 'react';  //react is a core library for developing UI application
 
-let viewStyle={
-    marginTop:100,
-    marginLeft:100,
-    backgroundColor: '#FC0'
-};
-let titleStyle={
-    fontSize:40
-}
+import {Text,View,Button,SafeAreaView,StyleSheet} from 'react-native';  //is a react based library for developing mobile UI
 
 
-export default function () {
+//we should call the supplied property as props convnetionally.
+//you can name it whatever you like. But you should name it props
 
-    let title='Book App';
+function AppHeader(props){
 
-    let subTitle='Book List';
+   
 
     return (
-            <View style={viewStyle} >
-                <Text style={titleStyle}>{title}</Text>
+      <View style={styles.headerContainer}>
+           <Text style={styles.headerStyle}>{props.heading}</Text>
+           <Text style={styles.dateStyle}>{props.displayDate ? new Date().toDateString().toUpperCase() : ''}</Text>
+      </View>
+    );
+}
 
-                <Text style={{color:'red'}}>{subTitle}</Text>
-            </View>
+
+const styles=StyleSheet.create({
+
+    container:{
+        
+        backgroundColor:'crimson',
+        height:"100%"
+        
+    },
+    headerContainer:{
+        paddingTop:40,
+        backgroundColor:'black',
+        paddingVertical:10,
+        paddingLeft:5
+    },
+    buttonStyle:{
+        margin:50,
+        color:'red'
+    },
+
+    headerStyle:{
+        color:'#FC0',
+        fontSize:30,
+        marginTop:20,
+        fontFamily:'script'
+  
+  
+      },
+     
+     dateStyle:{
+        textAlign:'right',
+        fontSize:12,
+        fontWeight:'bold',
+        fontFamily:'courier',
+        color:'white'
+      },
+      title:{
+          marginTop:10,
+          color:'white',
+          fontSize:30,
+          fontFamily:'script',
+          textAlign:'center',
+          fontWeight:'bold'
+      },
+      label:{
+
+          fontSize:20,
+          color:'white',
+          marginRight:20
+      },
+      valueText:{
+          fontSize:22,
+          color:'white',
+          fontWeight:'bold'
+      },
+      row:{
+          
+          marginVertical:10,
+          marginHorizontal:15,
+          flexDirection:'row'
+      }
+
+});
+
+
+const LabeledValue=({label,value})=>(
+    <View style={styles.row}>
+        <Text style={styles.label}>{label}</Text>
+        <Text style={styles.valueText}>{value}</Text>
+    </View>
+);
+
+
+const BookDetails=({title,author,price,rating,votes})=>{
+
+
+
+   return (
+     <View style={styles.container}>
+         <Text style={styles.title}>{title}</Text>
+        
+        <LabeledValue label='Author' value={author} />
+        <LabeledValue label='Price' value={`Rs ${price}`} />
+        <LabeledValue label='rating' value={`${rating} out of 5 (${votes} votes)`} />
+         
+         <Button style={styles.buttonStyle} title='Add to Favorite' />
+     </View>
    );
 }
+
+
+
+//{heading:'All About Books' , displayDate:false}
+
+const App= ()=>(    
+
+  <View style={styles.container}>
+              <AppHeader  heading='All About Books' displayDate={true} />
+              <BookDetails 
+                        title='The Accursed God'
+                        author='Vivek Dutta Mishra'
+                        price={336}
+                        rating={4.2}
+                        votes={80}
+                        />              
+  </View>
+
+); 
+
+
+
+export default App;
+
+
+
+
+
+
